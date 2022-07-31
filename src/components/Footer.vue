a037cff06eccf2b0487cbf5c631387df4f09f2bb
duowenbo <duowenbo@hatach.com.cn>
2022-07-31 16:49:29 +0800

    chore: init

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav text-xl mt-6>
+    <RouterLink class="icon-btn mx-2" to="/" title="主页">
+      <div i-carbon-campsite />
+    </RouterLink>
+
+    <button class="icon-btn mx-2 !outline-none" title="切换颜色" @click="toggleDark()">
+      <div i="carbon-sun dark:carbon-moon" />
+    </button>
+
+
+    <RouterLink class="icon-btn mx-2" to="/about" title="关于">
+      <div i-carbon-dicom-overlay />
+    </RouterLink>
+
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+      <div i-carbon-logo-github />
+    </a>
+  </nav>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const user = useUserStore()
+const name = $ref(user.savedName)
+
+const router = useRouter()
+const go = () => {
+  if (name)
+    router.push(`/hi/${encodeURIComponent(name)}`)
+}
+</script>
+
+<template>
+  <div>
+    <div text-4xl>
+      <div i-carbon-campsite inline-block />
+    </div>
+    <p>
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
+        Vitesse
+      </a>
+    </p>
+    <p>
+      <em text-sm opacity-75>描述</em>
+    </p>
+
+    <div py-4 />
+
+    <input
+      id="input"
+      v-model="name"
+      placeholder="请输入名称"
+      aria-label="请输入名称"
+      type="text"
+      autocomplete="false"
+      p="x4 y2"
+      w="250px"
+      text="center"
+      bg="transparent"
+      border="~ rounded gray-200 dark:gray-700"
+      outline="none active:none"
+      @keydown.enter="go"
+    >
+    <label class="hidden" for="input">输入名称</label>
+
+    <div>
+      <button
+        btn m-3 text-sm
+        :disabled="!name"
+        @click="go"
+      >
+        go
+      </button>
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
